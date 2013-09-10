30cd1bcf3eb85e691553245dd2ad0339a9e39590
Emil Wall <emil.wall@valtech.se>
2013-09-10 14:46:48 +0200

    Make sure reverse handles more base cases

--- spec.js
+++ spec.js
@@ -1,7 +1,7 @@
 var Reverser = function () {
   return {
-    reverse: function () {
-      return "";
+    reverse: function (arg) {
+      return arg;
     }
   };
 };
@@ -13,5 +13,9 @@ describe("string-reverse", function () {
 
   it("should return an empty string when passed an empty string", function () {
     expect(reverser.reverse("")).to.equal("");
+  });
+
+  it("should return its argument when passed a single character string", function () {
+    expect(reverser.reverse("a")).to.equal("a");
   });
 });
